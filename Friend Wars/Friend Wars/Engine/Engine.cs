ab4bba97960002473ad76fd97addf1025c7a1170
menohack <menohack@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-02 01:41:06 +0000

    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@4 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ Friend Wars/Friend Wars/Engine/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+public class Engine
+{
+    private DateTime lastUpdate;
+    TextBlock time;
+
+	public Engine(Canvas canvas)
+	{
+        DispatcherTimer timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.Zero;
+        timer.Tick += new EventHandler(tickEvent);
+        timer.Start();
+
+        time = new TextBlock();
+
+        canvas.Children.Add(time);
+
+        BitmapSource bitmap = new BitmapImage(new Uri("Resources//GunboundTitleScreen.bmp"));
+        WriteableBitmap luis = new WriteableBitmap(bitmap);
+        Window wind = new Window();
+
+    }
+
+    void Update()
+    {
+        DateTime now = DateTime.Now;
+        TimeSpan elapsed = now - lastUpdate;
+        lastUpdate = now;
+        time.Text = elapsed.ToString();
+    }
+
+    public void tickEvent(object sender, EventArgs e)
+    {
+        Update();
+    }
+}
