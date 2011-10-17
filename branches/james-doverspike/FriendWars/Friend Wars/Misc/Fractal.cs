8b10a86b7063fab9a22fc34520ae34505525d8a9
menohack <menohack@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-17 20:36:15 +0000

    Added mandelbrot, got mandelbox working.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@46 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- branches/james-doverspike/FriendWars/Friend Wars/Misc/Fractal.cs
+++ branches/james-doverspike/FriendWars/Friend Wars/Misc/Fractal.cs
@@ -20,16 +20,16 @@ using System.Windows.Media.Imaging;
         public Fractal(Image fractalImage)
         {
             //BitmapSource src = (BitmapSource) fractalImage.Source;
-            int width = 200;
-            int height = 200;
+            int width = (int)fractalImage.Width;
+            int height = (int)fractalImage.Height;
             WriteableBitmap wb = new WriteableBitmap(width, height);
             //Color color = new Color();
             //color.B = new byte();
 
-            double leftBound = -20.0;
-            double rightBound = 20.0;
-            double upBound = 10.0;
-            double downBound = -10.0;
+            double leftBound = -2.5;
+            double rightBound = 2.5;
+            double upBound = 1.0;
+            double downBound = -1.0;
             
 
             for (int j = 0; j < wb.PixelHeight; j++)
@@ -38,10 +38,11 @@ using System.Windows.Media.Imaging;
                 {
                     double x = leftBound + (i / (double)width) * (rightBound - leftBound);
                     double y = downBound + (j / (double)height) * (upBound - downBound);
-                    if (mandelbox(x, y, width, height, 2.0, 0.5, 1.0, 0.0, 25))
-                        wb.Pixels[j * wb.PixelWidth + i] = ConvertToARGB32(Color.FromArgb(255, 0, 0, 0));
-                    else
-                        wb.Pixels[j * wb.PixelWidth + i] = ConvertToARGB32(Color.FromArgb(255, 255, 255, 255));
+                    double value = fractal(x, y, width, height, 2.0, 0.5, 1.0, 200) * 255;
+                    //if ()
+                    wb.Pixels[j * wb.PixelWidth + i] = ConvertToARGB32(Color.FromArgb(255, (byte)value, (byte)value, (byte)value));
+                    //else
+                        //wb.Pixels[j * wb.PixelWidth + i] = ConvertToARGB32(Color.FromArgb(255, 255, 255, 255));
                 }
             }
 
@@ -50,45 +51,66 @@ using System.Windows.Media.Imaging;
             fractalImage.Source = wb;
         }
 
-        private bool mandelbox(double x, double y, int width, int height, double scale, double minRadius, double fixedRadius, double c, int iterations)
+        private double fractal(double x, double y, int width, int height, double scale, double minRadius, double fixedRadius, int iterations)
         {
             if (iterations < 0)
-                return false;
+                return 0.0;
 
 
             double[] v = new double[2];
-            v[0] = x;
-            v[1] = y;
+            v[0] = 0.0;
+            v[1] = 0.0;
 
-            for (int iter = 0; iter < iterations; iter++)
+            double[] c = new double[2];
+            c[0] = x;
+            c[1] = y;
+
+            int iter = 0;
+            for (; iter < iterations; iter++)
             {
-                for (int axis = 0; axis < DIMENSION; axis++)
-                {
-                    if (v[axis] > 1.0)
-                        v[axis] = 2.0 - v[axis];
-                    else if (v[axis] < -1.0)
-                        v[axis] = -2.0 - v[axis];
-                }
 
-                if (magnitude(v) < minRadius)
-                {
-                    v[0] *= (fixedRadius * fixedRadius) / (minRadius * minRadius);
-                    v[1] *= (fixedRadius * fixedRadius) / (minRadius * minRadius);
-                }
-                else if (magnitude(v) < fixedRadius)
-                {
-                    v[0] *= (fixedRadius * fixedRadius) / (magnitude(v) * magnitude(v));
-                    v[1] *= (fixedRadius * fixedRadius) / (magnitude(v) * magnitude(v));
-                }
-
-                v[0] = scale * v[0] + c;
-                v[1] = scale * v[1] + c;
-
+                v = mandelbox(v, c, scale, minRadius, fixedRadius);
+                //v = mandelbrot(v, c);
                 if (magnitude(v) > EPSILON)
-                    return false;
+                    return iter/(double)iterations;
             }
 
-            return true;
+            return 1.0;
+        }
+
+        private double[] mandelbrot(double[] v, double[] c)
+        {
+            v[0] = v[0] * v[0] - v[1] * v[1] + c[0];
+            v[1] = v[1] * v[0] + v[0] * v[1] + c[1];
+
+            return v;
+        }
+
+        private double[] mandelbox(double[] v, double[] c, double scale, double minRadius, double fixedRadius)
+        {
+            for (int axis = 0; axis < DIMENSION; axis++)
+            {
+                if (v[axis] > 1.0)
+                    v[axis] = 2.0 - v[axis];
+                else if (v[axis] < -1.0)
+                    v[axis] = -2.0 - v[axis];
+            }
+
+            if (magnitude(v) < minRadius)
+            {
+                v[0] *= (fixedRadius * fixedRadius) / (minRadius * minRadius);
+                v[1] *= (fixedRadius * fixedRadius) / (minRadius * minRadius);
+            }
+            else if (magnitude(v) < fixedRadius)
+            {
+                v[0] *= (fixedRadius * fixedRadius) / (magnitude(v) * magnitude(v));
+                v[1] *= (fixedRadius * fixedRadius) / (magnitude(v) * magnitude(v));
+            }
+
+            v[0] = scale * v[0] + c[0];
+            v[1] = scale * v[1] + c[1];
+
+            return v;
         }
 
         private double magnitude(double[] v)
