b6855734b49ca9a47ec0e1ba96fe15f433c8ba57
schiffhauera <schiffhauera@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-25 19:35:36 +0000

    - Implemented AudioComponent.  C# does Generics really weird (badly).  Not sure if I completely understand how to access an item from a ICollection<KeyValuePair<TKey, TValue>> without having to iterate through a bunch of things.  It appears that that interface does not, for some odd reason, implement a Get(TKey) function.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@64 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/Engine/Component/AudioComponent.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Component/AudioComponent.cs
@@ -9,15 +9,79 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Friendly_Wars.Engine.Object;
+using System.Collections.Generic;
 
 namespace Friendly_Wars.Engine.Component
 {
+    /// <summary>
+    /// Handles the control of a GameObject's audio.
+    /// </summary>
     public class AudioComponent
     {
+        /// <summary>
+        /// A Collection between the names of MediaElements and the actual MediaElement.
+        /// </summary>
+        ICollection<KeyValuePair<String, MediaElement>> audioClips;
 
+        /// <summary>
+        /// The constructor for an AudioComponent.
+        /// </summary>
+        /// <param name="owner">The owner of this AudioComponent.</param>
         public AudioComponent(GameObject owner)
         {
+            audioClips = new Dictionary<String, MediaElement>();
+        }
 
+        /// <summary>
+        /// Adds an audio clip to this AudioComponent.
+        /// </summary>
+        /// <param name="name">The name of the audio clip.</param>
+        /// <param name="audioClip">The audio clip, in the form of a MediaElement. </param>
+        public void AddAudioClip(String name, MediaElement audioClip) {
+            audioClips.Add(new KeyValuePair<String, MediaElement>(name, audioClip));
+        }
+
+        /// <summary>
+        /// Removes an audio clip from this AudioComponent.
+        /// </summary>
+        /// <param name="name">The name of the audio clip to remove.</param>
+        public void RemoveAudioClip(String name)
+        {
+            foreach (KeyValuePair<String, MediaElement> audioClip in audioClips) {
+                if (audioClip.Key.ToString() == name) {
+                    audioClips.Remove(new KeyValuePair<String, MediaElement>(audioClip.Key, audioClip.Value));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Plays a specific audio clip.
+        /// </summary>
+        /// <param name="name">The name of the audio clip to play.</param>
+        public void Play(String name)
+        {
+            foreach (KeyValuePair<String, MediaElement> audioClip in audioClips)
+            {
+                if (audioClip.Key.ToString() == name)
+                {
+                    audioClip.Value.Play();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stops playing a specific audio clip.
+        /// </summary>
+        /// <param name="name">The name of the audio clip to stop playing.</param>
+        public void Stop(String name)
+        {
+            foreach (KeyValuePair<String, MediaElement> audioClip in audioClips)
+            {
+                if (audioClip.Key.ToString() == name)
+                {
+                    audioClip.Value.Stop();
+                }
+            }
         }
 
     }
