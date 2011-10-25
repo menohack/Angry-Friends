df19b5ef287ccbb37a060cf1f5552674aefe7ab4
schiffhauera <schiffhauera@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-25 18:16:01 +0000

    - World now updates properly is frame-rate independent.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@59 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/Engine/Object/World.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Object/World.cs
@@ -10,6 +10,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Collections.Generic;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace Friendly_Wars.Engine.Object
 {
@@ -30,10 +31,19 @@ namespace Friendly_Wars.Engine.Object
         private DispatcherTimer timer;
 
         /// <summary>
-        /// 60 FPS, or 17 ms between each frame.  Might not be able to actually run at 60 FPS.
-        /// TODO: Figure out how to call a function as quickly as possible.
+        /// 30 FPS, or 33 ms between each frame. 
         /// </summary>
-        private static readonly int INTERVAL = 17;  
+        private static readonly int INTERVAL = 33;
+
+        /// <summary>
+        /// The DateTime associated with the last frame.
+        /// </summary>
+        private DateTime previousTime;
+
+        /// <summary>
+        /// The change in time from the previous frame, in miliseconds.
+        /// </summary>
+        private int deltaTime = 0;
 
         /// <summary>
         /// The queue of GameObjects that need to be re-drawn/updated the next time WordObject updates.
@@ -53,16 +63,33 @@ namespace Friendly_Wars.Engine.Object
         public World(String name, String tag = null) : base(name, tag) {
             redrawQueue = new List<GameObject>();
             updateableGameObjects = new List<GameObject>();
+            previousTime = DateTime.Now;
 
+            // Begin updating the world.
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(INTERVAL);
-            timer.Tick += new EventHandler(timer_Update);
+            timer.Tick += new EventHandler(Update);
             timer.Start();
         }
 
-        void timer_Update(object sender, EventArgs e)
+        /// <summary>
+        /// Updates the World as fast as possible; updating is capped at 1000/INTERVAL times per second.
+        /// The Update is frame-rate-independent and will account for a drop in frame rate.
+        /// </summary>
+        /// <param name="sender">The Object that called this function.</param>
+        /// <param name="e">The event that corresponds to this function.</param>
+        void Update(object sender, EventArgs e)
         {
-            System.Console.WriteLine("Hello, World!");
+            DateTime currentTime = DateTime.Now;
+
+            deltaTime = currentTime.Millisecond - previousTime.Millisecond;
+            // If we elapsed one second
+            if (deltaTime <= 0)
+            {
+                deltaTime = 1000 - previousTime.Millisecond + currentTime.Millisecond;
+            }
+
+            previousTime = DateTime.Now;
         }
     }
 }
