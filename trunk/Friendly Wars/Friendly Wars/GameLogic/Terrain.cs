733ab1efe6e365ea79db55f856287b004f5e2dfd
schiffhauera <schiffhauera@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-26 17:51:20 +0000

    - Changes to Terrain. - Now compiles.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@71 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/GameLogic/Terrain.cs
+++ trunk/Friendly Wars/Friendly Wars/GameLogic/Terrain.cs
@@ -11,73 +11,37 @@ using System.Windows.Shapes;
 using System.Windows.Media.Imaging;
 
 using Friendly_Wars.Engine.Object;
+using Friendly_Wars.Engine.Ultilities;
 
 namespace Friendly_Wars.GameLogic
 {
     /// <summary>
     /// The Terrain class represents the deformable ground.
     /// </summary>
-    public class Terrain
+    public class Terrain : GameObject, IUpdateable
     {
-        /** private WriteableBitmap bitmap;
-
          /// <summary>
-         /// Constructs a Terrain object.
+         /// Constructor for a new instance of Terrain.
          /// </summary>
          /// <param name="image"></param>
-         public Terrain(Image image, String name, String tag = null) : base(name, tag)
+         public Terrain(String name) : base(name)
          {
-             renderComponent.Bitmap = image;
-
-             int width = (int) renderComponent.Bitmap.Width * 2;
-             int height = (int) renderComponent.Bitmap.Height * 2;
-
-             WriteableBitmap bitmap = new WriteableBitmap(width, height);
-
-             ///Initialize the terrain
-             for (int i = 0; i < width; i++)
-             {
-                 for (int j=0; j < height; j++) {
-                     if (j > 3*height/4 + Math.Sin(i/(8.0*Math.PI)) * (height/8))
-                         bitmap.Pixels[j*width + i] = ConvertToARGB32(Color.FromArgb(255, 0, 50, 200));
-                     else
-                         bitmap.Pixels[j * width + i] = ConvertToARGB32(Color.FromArgb(255, 0, 200, 200));
-                 }
-             }
-
-
-             bitmap.Invalidate();
-
-             renderComponent.Bitmap.Source = bitmap;
+            
          }
 
          /// <summary>
          /// This function checks if the GameObject is colliding with the terrain.
          /// </summary>
-         /// <param name="gameObject"> The colliding object. </param>
+         /// <param name="gameObject"> The GameObject that collided with this Terrain. </param>
          public bool Collide(GameObject gameObject)
          {
              return physicsComponent.Collide(gameObject);
          }
 
-         /// <summary>
-         /// Updates the terrain.
-         /// </summary>
-         public override void Update()
-         {
-             ///TODO: update the terrain
-         }
-
-         /// <summary>
-         /// Helper method for converting Colors to ARGB32 integers.
-         /// </summary>
-         /// <param name="color"> The Color to be converted. </param>
-         /// <returns> The Color as an integer. </returns>
-         private int ConvertToARGB32(Color color)
-         {
-             return ((color.R << 16) | (color.G << 8) | (color.B << 0) | (color.A << 24));
-         }
-
-     }*/
+        
+        public void Update(double deltaTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
