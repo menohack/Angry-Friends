2ef934fcebb89d422a24b912479d2a78afb641c4
schiffhauera <schiffhauera@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-27 01:23:15 +0000

    - Some changes for Game.  Now threading from Game, not MainPage.xaml.cs.
    Should no longer edit MainPage.xaml.cs.
    
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@89 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/GameLogic/Game.cs
+++ trunk/Friendly Wars/Friendly Wars/GameLogic/Game.cs
@@ -10,32 +10,37 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 using Friendly_Wars.Engine.Object;
+using Friendly_Wars.Engine.Ultilities;
 
 namespace Friendly_Wars.GameLogic
 {
-    /// <summary>
-    /// This class represents the Game. All game logic stems from here.
-    /// </summary>
-    public class Game
-    {
-        /// <summary>
-        /// The world of the game.
-        /// </summary>
-        private World world;
-        /// <summary>
-        /// The terrain of the game.
-        /// </summary>
-        private Terrain terrain;
+	/// <summary>
+	/// This class represents the Game. All game-logic stems from here.
+	/// </summary>
+	public class Game
+	{
+		/// <summary>
+		/// The world of the game.
+		/// </summary>
+		private World world;
 
-        /// <summary>
-        /// The constructor for the Game.
-        /// </summary>
-        /// <param name="image"> The image used for the terrain. </param>
-        public Game(Image image)
-        {
-            world = new World(World.WORLD_NAME);
+		/// <summary>
+		/// The terrain of the game.
+		/// </summary>
+		public Terrain terrain {get; private set; }
 
-           // terrain = new Terrain(image, "terrain");
-        }
-    }
+		/// <summary>
+		/// The Web of the game.
+		/// </summary>
+		public Web web { get; private set; }
+
+		/// <summary>
+		/// Constructor for the Game.
+		/// </summary>
+		public Game()
+		{
+			world = new World(World.WORLD_NAME);
+			web = new Web();
+		}
+	}
 }
--- trunk/Friendly Wars/Friendly Wars/MainPage.xaml.cs
+++ trunk/Friendly Wars/Friendly Wars/MainPage.xaml.cs
@@ -1,24 +1,25 @@
 ﻿using System.Windows.Controls;
 using Friendly_Wars.Engine.Ultilities;
 using Friendly_Wars.Engine.Object;
+using Friendly_Wars.GameLogic;
 
 namespace Friendly_Wars {
 	public partial class MainPage : UserControl {
 
-		public static Grid canvas;
+		/// <summary>
+		/// A reference to the instance of our MainPage; this way, we can access non-static fields.
+		/// </summary>
+		public static MainPage mainPage;
+
 		/// <summary>
 		/// This class represents our Silverlight page.
 		/// </summary>
 		public MainPage() {
 			InitializeComponent();
-			new World(World.WORLD_NAME);
-			new Web();
-			//var map = MediaLoader.LoadMapXml("file:///C:/Users/Luis%20Grimaldo/Desktop/oose-game/trunk/Friendly%20Wars/Friendly%20Wars/maps/beach.xml");
-			//; ; ;
-			//var image = new Image();
-			//image.Source = Web.DownloadImage("http://www.google.com/intl/en_com/images/srpr/logo3w.png");
-			//LayoutRoot.Children.Add(image);
-			canvas = LayoutRoot;
+			mainPage = this;
+
+			// HOOK
+			new Game();
 		}
 	}
 }
