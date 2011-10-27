64150e9dadee0cac5c4dbe1c30c75ac0a7ce8772
luis <luis@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-27 03:32:04 +0000

    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@98 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ trunk/Friendly Wars/Friendly Wars/Tests/Test_MediaLoader.cs
@@ -0,0 +1,27 @@
+﻿using Friendly_Wars.Engine.Utilities;
+namespace Friendly_Wars.Tests {
+	/// <summary>
+	/// Test for loading media
+	/// </summary>
+	public static class Test_MediaLoader {
+		/// <summary>
+		/// Runs the test
+		/// </summary>
+		public static void RunTest() {
+			new MediaLoader();
+			string test_case = @"
+				<map title='beach'>
+					<resources>
+						<sound url='explosion_0.wav'/>
+						<image url='moving_1.png'/>
+						<image url='moving_2.png'/>
+						<image url='moving_3.png'/>
+						<image url='moving_4.png'/>
+						<image url='moving_5.png'/>
+					</resources>
+				</map>
+				";
+			MediaLoader.LoadMapXml(test_case);
+		}
+	}
+}
---
+++ trunk/Friendly Wars/Friendly Wars/Tests/Test_Web.cs
@@ -0,0 +1,17 @@
+﻿using Friendly_Wars.Engine.Utilities;
+namespace Friendly_Wars.Tests {
+	/// <summary>
+	/// Test for downloading media
+	/// </summary>
+	public static class Test_Web {
+		/// <summary>
+		/// Runs the test
+		/// </summary>
+		public static void RunTest() {
+			new Web();
+			Web.DownloadImage("http://www.google.com/intl/en_com/images/srpr/logo3w.png");
+			Web.DownloadString("http://www.google.com");
+			Web.DownloadSound("http://www.choosah.com/example.wav");
+		}
+	}
+}
