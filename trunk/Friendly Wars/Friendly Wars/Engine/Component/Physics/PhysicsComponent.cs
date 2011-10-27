fb7e2ffc21f3567d53eeed9bf64fc147e466d73a
schiffhauera <schiffhauera@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-27 01:29:32 +0000

    - Getting rid of a lot of "using" statements
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@90 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/Engine/Component/Physics/PhysicsComponent.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Component/Physics/PhysicsComponent.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Friendly_Wars.Engine.Object;
-using System.Collections;
 
 namespace Friendly_Wars.Engine.Component.Physics
 {
--- trunk/Friendly Wars/Friendly Wars/Engine/Object/GameObject.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Object/GameObject.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Net;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-
 using Friendly_Wars.Engine.Component;
 using Friendly_Wars.Engine.Component.Graphic;
 using Friendly_Wars.Engine.Component.Physics;
-using System.Collections.Generic;
 
 namespace Friendly_Wars.Engine.Object
 {
