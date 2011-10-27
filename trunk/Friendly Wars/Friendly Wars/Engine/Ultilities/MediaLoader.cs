87b47069e65a69c3250841ef05770c27f7c4dd25
luis <luis@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-27 02:36:21 +0000

    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@94 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/Engine/Ultilities/MediaLoader.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Ultilities/MediaLoader.cs
@@ -4,35 +4,55 @@ using System.Windows.Controls;
 using System.Xml;
 using Friendly_Wars.GameLogic;
 namespace Friendly_Wars.Engine.Ultilities {
+	/// <summary>
+	/// Loads the media
+	/// </summary>
 	public class MediaLoader {
-		public static Dictionary<string, MediaElement> mediaElements;
-		public MediaLoader() {
-			mediaElements = new Dictionary<string, MediaElement>();
-		}
+		/// <summary>
+		/// Loads a map's xml description
+		/// </summary>
+		/// <param name="filename">XML filename</param>
+		/// <returns>Map Information</returns>
 		public static MapInfo LoadMapXml(string filename) {
-			XmlReader reader = XmlReader.Create(new StringReader(Web.DownloadString(filename)));
-			MapInfo mapInfo = new MapInfo();
-			var path = Web.Path + "maps/";
-			reader.ReadToFollowing("map");
-			while (reader.MoveToNextAttribute()) {
-				switch (reader.Name) {
-					case "title": mapInfo.Title = reader.Value; break;
-					case "resources":
-						if (reader.HasAttributes) {
-							do {
+			//XmlReader reader = XmlReader.Create(new StringReader(Web.DownloadString(filename)));
+			using (XmlReader reader = XmlReader.Create(new StringReader(@"
+				<map title='beach'>
+					<resources>
+						<sound url='explosion_0.wav'/>
+						<image url='moving_1.png'/>
+						<image url='moving_2.png'/>
+						<image url='moving_3.png'/>
+						<image url='moving_4.png'/>
+						<image url='moving_5.png'/>
+					</resources>
+				</map>
+				"))) {
+				MapInfo mapInfo = new MapInfo();
+				var path = Web.Path + "maps/";
+				reader.ReadToFollowing("map");
+				reader.MoveToAttribute("title");
+				mapInfo.Title = reader.Value;
+				reader.MoveToContent();
+				reader.Read();
+				while (reader.Read()) {
+					switch (reader.Name) {
+						case "resources":
+							while (reader.Read()) {
+								reader.Read();
+								string type = reader.Name;
+								reader.MoveToAttribute("url");
 								string name = reader.Value.Contains(".") ? reader.Value.Split('.')[0] : reader.Value;
 								string url = path + "resources/" + reader.Value;
-								switch (reader.Name) {
+								switch (type) {
 									case "image": mapInfo.Images.Add(name, Web.DownloadImage(url)); break;
 									case "sound": mapInfo.Sounds.Add(name, Web.DownloadSound(url)); break;
 								}
 							}
-							while (reader.MoveToNextAttribute());
-						}
-						break;
+							break;
+					}
 				}
+				return mapInfo;
 			}
-			return mapInfo;
 		}
 	}
 }
--- trunk/Friendly Wars/Friendly Wars/Engine/Ultilities/Web.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Ultilities/Web.cs
@@ -12,14 +12,34 @@ using System.Windows.Media.Imaging;
 using System.Xml;
 using System.Threading;
 namespace Friendly_Wars.Engine.Ultilities {
+	/// <summary>
+	/// Download manager
+	/// </summary>
 	public class Web {
 		static WebClient webclient = new WebClient();
 		static AutoResetEvent mre = new AutoResetEvent(false);
+		/// <summary>
+		/// Constructor for the download manager
+		/// </summary>
 		public Web() {
 			webclient = new WebClient();
 		}
+		/// <summary>
+		/// Contains the server path
+		/// </summary>
 		public static string Path { get { return "file:///C:/Users/Luis%20Grimaldo/Desktop/oose-game/trunk/Friendly%20Wars/Friendly%20Wars/"; } }
+		/// <summary>
+		/// Downloads a string sync
+		/// </summary>
+		/// <param name="url">URL where the string is located</param>
+		/// <returns>The resulting string</returns>
 		public static string DownloadString(string url) { return DownloadString(url, null); }
+		/// <summary>
+		/// Downloads a string async/sync
+		/// </summary>
+		/// <param name="url">URL where the string is located</param>
+		/// <param name="onLoaded">Event to be fired once the download is completed</param>
+		/// <returns>The resulting string if sync, An empty string if async</returns>
 		public static string DownloadString(string url, Action<string> onLoaded) {
 			string result = string.Empty;
 			webclient.DownloadStringCompleted += (s, events) => {
@@ -34,9 +54,20 @@ namespace Friendly_Wars.Engine.Ultilities {
 			}
 			else return null;
 		}
+		/// <summary>
+		/// Downloads an image sync
+		/// </summary>
+		/// <param name="url">URL where the image is located</param>
+		/// <returns>The resulting image</returns>
 		public static BitmapImage DownloadImage(string url) { return DownloadImage(url, null); }
+		/// <summary>
+		/// Downloads an image async/sync
+		/// </summary>
+		/// <param name="url">URL where the image is located</param>
+		/// <param name="onLoaded">Event to be fired once the download is completed</param>
+		/// <returns>The resulting string</returns>
 		public static BitmapImage DownloadImage(string url, Action<BitmapImage> onLoaded) {
-			var image = new BitmapImage() { CreateOptions = BitmapCreateOptions.None };
+			var image = new BitmapImage(new Uri(url)) { CreateOptions = BitmapCreateOptions.None };
 			image.ImageOpened += (s, events) => {
 				if (onLoaded == null) mre.Set();
 				else onLoaded(image);
@@ -48,7 +79,18 @@ namespace Friendly_Wars.Engine.Ultilities {
 			}
 			else return null;
 		}
+		/// <summary>
+		/// Downloads a sound sync
+		/// </summary>
+		/// <param name="url">URL where the sound is located</param>
+		/// <returns>The resulting string</returns>
 		public static MediaElement DownloadSound(string url) { return DownloadSound(url, null); }
+		/// <summary>
+		/// Downloads a sound sync
+		/// </summary>
+		/// <param name="url">URL where the sound is located</param>
+		/// <param name="onLoaded">Event to be fired once the download is completed</param>
+		/// <returns>The resulting string</returns>
 		public static MediaElement DownloadSound(string url, Action<MediaElement> onLoaded) {
 			MediaElement media = new MediaElement();
 			media.Source = new Uri(url);
