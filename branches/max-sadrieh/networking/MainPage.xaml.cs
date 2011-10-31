523489e5f4859a2df27f2b47616d438831936205
max <max@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-10-31 23:03:16 +0000

    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@107 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/max-sadrieh/networking/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using Friendly_Wars.Engine.Utilities;
+using Friendly_Wars.Engine.Object;
+using Friendly_Wars.GameLogic;
+
+namespace Friendly_Wars {
+	public partial class MainPage : UserControl {
+
+		/// <summary>
+		/// A reference to the instance of our MainPage; this way, we can access non-static fields.
+		/// </summary>
+		public static MainPage mainPage;
+
+		/// <summary>
+		/// This class represents our Silverlight page.
+		/// </summary>
+		public MainPage() {
+			InitializeComponent();
+			mainPage = this;
+
+			// HOOK
+			new Game();
+		}
+	}
+}
