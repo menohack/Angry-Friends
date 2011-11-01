5376e72af2f51ceb54e8cb26ccf2708606c74b78
max <max@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-01 05:32:37 +0000

    Got rid of IFriend and instead consolidated it into ISocial by creating ISocialPerson. Added comments to the attributes and methods. Changed FacebookPerson accordingly.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@114 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/max-sadrieh/networking/Friendly Wars/Friendly Wars/Networking/FacebookPerson.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System;
+
+namespace Friendly_Wars.Networking
+{
+    public class FacebookPerson : ISocialPerson
+    {
+
+
+        /// <summary>
+        /// The unique identifier of the person, qualified by a namespace
+        /// </summary>
+        string Id { get; set; }
+
+        /// <summary>
+        /// The public name of the person
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// The list of friends of a person
+        /// </summary>
+        IList<ISocialPerson> Friends { get; set; }
+
+        /// <summary>
+        /// Creates a new FacebookPerson
+        /// </summary>
+        /// <param name="id"></param>
+        public FacebookPerson(string id)
+        {
+            if (id == null)
+                throw new ArgumentNullException("FacebookID cannot be null");
+            if (String.IsNullOrWhiteSpace(id))
+                throw new ArgumentException("FacebookID cannot be empty");
+
+            //FIXME: Further testing by querying Facebook
+
+            this.Id = id;
+        }
+
+        /// <summary>
+        /// Publishes a message on behalf of a person
+        /// </summary>
+        /// <param name="message"></param>
+        void Publish(string message)
+        {
+        }
+
+        /// <summary>
+        /// Makes a request to a friend of the person on his or her behalf
+        /// </summary>
+        /// <param name="friend"></param>
+        void MakeRequest(ISocialPerson friend)
+        {
+        }
+    }
+}
---
+++ branches/max-sadrieh/networking/Friendly Wars/Friendly Wars/Networking/ISocialPerson.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Friendly_Wars.Networking
+{
+    public interface ISocialPerson
+    {
+        /// <summary>
+        /// The unique identifier of the person, qualified by a namespace
+        /// </summary>
+        string Id { get; set; }
+
+        /// <summary>
+        /// The public name of the person
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// The list of friends of a person
+        /// </summary>
+        IList<ISocialPerson> Friends { get; set; }
+
+        /// <summary>
+        /// Publishes a message on behalf of a person
+        /// </summary>
+        /// <param name="message"></param>
+        void Publish(string message);
+
+        /// <summary>
+        /// Makes a request to a friend of the person on his or her behalf
+        /// </summary>
+        /// <param name="friend"></param>
+        void MakeRequest(ISocialPerson friend);
+
+    }
+}
