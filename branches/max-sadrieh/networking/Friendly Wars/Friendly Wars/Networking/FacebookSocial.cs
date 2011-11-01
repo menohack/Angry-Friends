d892985571843cb5f40b73b100d70e84fc7fc380
max <max@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-01 05:00:50 +0000

    Fixed multiple syntax/semantic errors in ISocial. Added IFriend. Added an implementation of ISocial (FacebookSocial). Added some extremely basic unit tests for FacebookSocial.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@113 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/max-sadrieh/networking/Friendly Wars/Friendly Wars/Networking/FacebookSocial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System;
+
+namespace Friendly_Wars.Networking
+{
+    public class FacebookSocial : ISocial
+    {
+        string id;
+
+        public FacebookSocial(string id)
+        {
+            if (id == null)
+                throw new ArgumentNullException("FacebookID cannot be null");
+            if (String.IsNullOrWhiteSpace(id))
+                throw new ArgumentException("FacebookID cannot be empty");
+
+            this.id = id;
+        }
+
+        public IList<IFriend> GetFriends()
+        {
+            return null;
+        }
+
+        public void Publish(string message)
+        {
+        }
+
+        public void MakeRequest(IFriend friend)
+        {
+        }
+    }
+}
---
+++ branches/max-sadrieh/networking/Friendly Wars/TestFriendlyWars/FacebookSocialTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Friendly_Wars.Networking;
+
+namespace TestFriendlyWars
+{
+    [TestClass]
+    public class FacebookSocialTest
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void TestOnNullId()
+        {
+            new FacebookSocial(null);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void TestOnEmptyId()
+        {
+            new FacebookSocial(String.Empty);
+        }
+
+        [TestMethod]
+        public void TestCorrectNiceId()
+        {
+            FacebookSocial fs = new FacebookSocial("msadrieh");
+            Assert.IsNotNull(fs);
+            Assert.IsInstanceOfType(fs, typeof(FacebookSocial));
+        }
+
+        [TestMethod]
+        public void TestCorrectUglyId()
+        {
+            FacebookSocial fs = new FacebookSocial("1225470476");
+            Assert.IsNotNull(fs);
+            Assert.IsInstanceOfType(fs, typeof(FacebookSocial));
+        }
+    }
+}
