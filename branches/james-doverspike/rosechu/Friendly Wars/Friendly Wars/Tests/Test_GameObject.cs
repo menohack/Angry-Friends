61431819822a927aacb16b964b22c29f13b468f3
menohack <menohack@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-05 04:55:05 +0000

    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@122 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/james-doverspike/rosechu/Friendly Wars/Friendly Wars/Tests/Test_GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Friendly_Wars.Engine.Object;
+
+namespace Friendly_Wars.Tests
+{
+	/// <summary>
+	/// Memory test for GameObjects.
+	/// </summary>
+	public class Test_GameObject : ITestable
+	{
+		/// <summary>
+		/// Tests to see if the World can handle 10000 GameObjects
+		/// </summary>
+		/// <returns>True if the test works.</returns>
+		public bool RunTest()
+		{
+			for (int i = 0; i < 10000; i++)
+			{
+				new GameObject(i.ToString());
+			}
+			return true;
+		}
+	}
+}
