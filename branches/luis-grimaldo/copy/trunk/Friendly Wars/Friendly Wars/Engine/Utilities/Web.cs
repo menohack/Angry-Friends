94b2563feef4d975b3e5d97ff39808aee1b18a9c
luis <luis@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-07 22:56:59 +0000

    Downloader
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@129 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/luis-grimaldo/copy/trunk/Friendly Wars/Friendly Wars/Engine/Utilities/Web.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+using System.Xml;
+using System.Threading;
+namespace Friendly_Wars.Engine.Utilities {
+	/// <summary>
+	/// Download manager
+	/// </summary>
+	public class Web {
+		static WebClient webclient = new WebClient();
+		static ManualResetEvent mre = new ManualResetEvent(false);
+		/// <summary>
+		/// Constructor for the download manager
+		/// </summary>
+		public Web() {
+			webclient = new WebClient();
+		}
+		/// <summary>
+		/// Contains the server path
+		/// </summary>
+		public static string Path { get { return "file:///C:/Users/Luis%20Grimaldo/Desktop/oose-game/trunk/Friendly%20Wars/Friendly%20Wars/"; } }
+		/// <summary>
+		/// Downloads a string sync
+		/// </summary>
+		/// <param name="url">URL where the string is located</param>
+		/// <returns>The resulting string</returns>
+		public static string DownloadString(string url) { return DownloadString(url, null); }
+		/// <summary>
+		/// Downloads a string async/sync
+		/// </summary>
+		/// <param name="url">URL where the string is located</param>
+		/// <param name="onLoaded">Event to be fired once the download is completed</param>
+		/// <returns>The resulting string if sync, An empty string if async</returns>
+		public static string DownloadString(string url, Action<string> onLoaded) {
+			string result = string.Empty;
+			webclient.DownloadStringCompleted += (s, events) => {
+				result = events.Result;
+				if (onLoaded == null) mre.Set();
+				else onLoaded(result);
+			};
+			webclient.DownloadStringAsync(new Uri(url));
+			if (onLoaded == null) {
+				mre.WaitOne(1);
+				return result;
+			}
+			else return null;
+		}
+		/// <summary>
+		/// Downloads an image sync
+		/// </summary>
+		/// <param name="url">URL where the image is located</param>
+		/// <returns>The resulting image</returns>
+		public static BitmapImage DownloadImage(string url) { return DownloadImage(url, null); }
+		/// <summary>
+		/// Downloads an image async/sync
+		/// </summary>
+		/// <param name="url">URL where the image is located</param>
+		/// <param name="onLoaded">Event to be fired once the download is completed</param>
+		/// <returns>The resulting string</returns>
+		public static BitmapImage DownloadImage(string url, Action<BitmapImage> onLoaded) {
+			var image = new BitmapImage(new Uri(url)) { CreateOptions = BitmapCreateOptions.None };
+			image.ImageOpened += (s, events) => {
+				if (onLoaded == null) mre.Set();
+				else onLoaded(image);
+			};
+			image.UriSource = new Uri(url);
+			if (onLoaded == null) {
+				mre.WaitOne(1);
+				return image;
+			}
+			else return null;
+		}
+		/// <summary>
+		/// Downloads a sound sync
+		/// </summary>
+		/// <param name="url">URL where the sound is located</param>
+		/// <returns>The resulting string</returns>
+		public static MediaElement DownloadSound(string url) { return DownloadSound(url, null); }
+		/// <summary>
+		/// Downloads a sound sync
+		/// </summary>
+		/// <param name="url">URL where the sound is located</param>
+		/// <param name="onLoaded">Event to be fired once the download is completed</param>
+		/// <returns>The resulting string</returns>
+		public static MediaElement DownloadSound(string url, Action<MediaElement> onLoaded) {
+			MediaElement media = new MediaElement();
+			media.Source = new Uri(url);
+			media.Loaded += (s, events) => {
+				if (onLoaded == null) mre.Set();
+				else onLoaded(media);
+			};
+			if (onLoaded == null) {
+				mre.WaitOne(1);
+				return media;
+			}
+			else return null;
+		}
+	}
+}
