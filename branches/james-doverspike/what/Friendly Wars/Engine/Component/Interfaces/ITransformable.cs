6e4fcd618aaee65223d98cc45a126566c26b00b9
menohack <menohack@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-09 21:51:48 +0000

    I don't know what's going on.
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@152 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/james-doverspike/what/Friendly Wars/Engine/Component/Interfaces/ITransformable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Friendly_Wars.Engine.Component.Interfaces
+{
+	/// <summary>
+	/// Represents an object that is transformable.
+	/// </summary>
+	public interface ITransformable
+	{
+		/// <summary>
+		/// Moves the TransformComponent by a specified amount.
+		/// </summary>
+		/// <param name="deltaPosition">The amount by which to move.</param>
+		void MoveBy(Point deltaPosition);
+
+		/// <summary>
+		/// Move the TransformComponent to a given value.
+		/// </summary>
+		/// <param name="absolutePosition">The absolute value in which to move.</param>
+		void MoveTo(Point absolutePosition);
+
+		/// <summary>
+		/// Rotates the TransformComponent by a given value.
+		/// </summary>
+		/// <param name="deltaRotation">The value by which to rotate.</param>
+		void RotateBy(int deltaRotation);
+
+		/// <summary>
+		/// Rotates the TransformComponent to a given value.
+		/// </summary>
+		/// <param name="absolutePosition">The value in which to rotate.</param>
+		void RotateTo(int absolutePosition);
+
+		/// <summary>
+		/// Resizes the TransformComponent by a given factor.
+		/// </summary>
+		/// <param name="deltaSize">The factor by which to resize.</param>
+		void ResizeBy(Point deltaSize);
+
+		/// <summary>
+		/// Resizes the TransformComponent to a specific size.
+		/// </summary>
+		/// <param name="absoluteSize">The size in which to resize.</param>
+		void ResizeTo(Point absoluteSize);
+	}
+}
