4f978b5d9ac76f1f360c50c08a25fb9b45b80f0f
schiffhauera <schiffhauera@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-09 23:01:05 +0000

    - Merging with trunk
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@163 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/alexander-schiffhauer/components/Friendly Wars/Engine/Utilities/EngineMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Friendly_Wars.Engine.Utilities
+{
+	/// <summary>
+	/// EngineMath process Engine-related math.
+	/// </summary>
+	public class EngineMath
+	{
+		/// <summary>
+		/// The cut-off value for an approximation.
+		/// </summary>
+		private static readonly Double EPSILON = .01;
+
+		/// <summary>
+		/// Determines if two Doubles are approximately the same value.
+		/// </summary>
+		/// <param name="firstNumber">The first number.</param>
+		/// <param name="secondNumber">The second number.</param>
+		/// <returns> True if the numbers are approximately the same. </returns>
+		public static bool Approximately(Double firstNumber, Double secondNumber)
+		{
+			if (Math.Abs(firstNumber - secondNumber) < EPSILON)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
