0d45257b787961f9b2593a632564c8a5d788629b
max <max@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-09 23:39:51 +0000

    Fixed up comments in Web to a certain extent...
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@165 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

--- trunk/Friendly Wars/Friendly Wars/Engine/Utilities/Web.cs
+++ trunk/Friendly Wars/Friendly Wars/Engine/Utilities/Web.cs
@@ -8,12 +8,22 @@ using System.Windows.Media.Imaging;
 using System.Xml;
 using Friendly_Wars.GameLogic;
 namespace Friendly_Wars.Engine.Utilities {
+
 	/// <summary>
-	/// Download manager
+	/// Download manager. Is responsible for loading media such as images, sound, text or maps.
+    /// Has the ability to download media either synchronously or asynchronously depending on the context and needs of the caller.
 	/// </summary>
 	public class Web {
 
+        /// <summary>
+        /// The single instance of Web.
+        /// </summary>
         private static Web instance;
+
+        /// <summary>
+        /// Accessor for the instance of Web.
+        /// This accessor follows the singleton pattern for C# provided at Microsoft's MSDN.
+        /// </summary>
         public static Web Instance
         {
             get
@@ -24,80 +34,104 @@ namespace Friendly_Wars.Engine.Utilities {
             }
         }
 
+        /// <summary>
+        /// The WebClient used to actually generate HTTP requests and process responses.
+        /// </summary>
 		WebClient webclient = new WebClient();
+        /// <summary>
+        /// 
+        /// </summary>
 		ManualResetEvent mre = new ManualResetEvent(false);
+
 		/// <summary>
-		/// Downloads a string async/sync
+		/// Downloads a string asynchronously.
 		/// </summary>
 		/// <param name="url">URL where the string is located</param>
 		/// <param name="onLoaded">Event to be fired once the download is completed</param>
-		/// <returns>The resulting string if sync, An empty string if async</returns>
 		public void DownloadString(string url, Action<string> onLoaded) {
 			webclient.DownloadStringCompleted += (s, events) => { onLoaded(events.Result); };
 			webclient.DownloadStringAsync(new Uri(url));
 		}
+
 		/// <summary>
-		/// Downloads an image sync
+		/// Downloads an image synchronously
 		/// </summary>
 		/// <param name="url">URL where the image is located</param>
 		/// <returns>The resulting image</returns>
-		public BitmapImage DownloadImage(string url) { return DownloadImage(url, null); }
+		public BitmapImage DownloadImage(string url)
+        {
+            return DownloadImage(url, null);
+        }
+
 		/// <summary>
-		/// Downloads an image async/sync
+		/// Downloads an image either synchronouly or asynchornously
 		/// </summary>
 		/// <param name="url">URL where the image is located</param>
-		/// <param name="onLoaded">Event to be fired once the download is completed</param>
-		/// <returns>The resulting string</returns>
-		public BitmapImage DownloadImage(string url, Action<BitmapImage> onLoaded) {
-			try {
-				var image = new BitmapImage(new Uri(url)) { CreateOptions = BitmapCreateOptions.None };
-				image.ImageOpened += (s, events) => {
-					if (onLoaded == null) mre.Set();
-					else onLoaded(image);
-				};
-				image.UriSource = new Uri(url);
-				if (onLoaded == null) {
-					mre.WaitOne(1);
-					return image;
-				}
+		/// <param name="onLoaded">Event to be fired once the download is completed or null</param>
+		/// <returns>The resulting image if onLoaded is null; otherwise null</returns>
+		public BitmapImage DownloadImage(string url, Action<BitmapImage> onLoaded)
+        {
+			BitmapImage image = new BitmapImage(new Uri(url)) { CreateOptions = BitmapCreateOptions.None };
+
+			image.ImageOpened += (s, events) =>
+            {
+				if (onLoaded == null) mre.Set();
+				else onLoaded(image);
+			};
+
+			image.UriSource = new Uri(url);
+
+            // If we are downloading the image synchronously, we wait 1 millisecond which we *GUESS* is enough time for the download FIXME FIXME FIXME!!!@!!@!@@!!11!11one
+			if (onLoaded == null)
+            {
+				mre.WaitOne(1);
+				return image;
 			}
-			catch { }
 			return null;
 		}
+
 		/// <summary>
-		/// Downloads a sound sync
+		/// Downloads a sound synchronously
 		/// </summary>
 		/// <param name="url">URL where the sound is located</param>
-		/// <returns>The resulting string</returns>
-		public MediaElement DownloadSound(string url) { return DownloadSound(url, null); }
+		/// <returns>The resulting sound</returns>
+		public MediaElement DownloadSound(string url)
+        {
+            return DownloadSound(url, null);
+        }
+
 		/// <summary>
-		/// Downloads a sound sync
+		/// Downloads a sound either synchronously or asynchronously
 		/// </summary>
 		/// <param name="url">URL where the sound is located</param>
-		/// <param name="onLoaded">Event to be fired once the download is completed</param>
-		/// <returns>The resulting string</returns>
-		public MediaElement DownloadSound(string url, Action<MediaElement> onLoaded) {
-			try {
-				MediaElement media = new MediaElement();
-				media.Source = new Uri(url);
-				media.Loaded += (s, events) => {
-					if (onLoaded == null) mre.Set();
-					else onLoaded(media);
-				};
-				if (onLoaded == null) {
-					mre.WaitOne(1);
-					return media;
-				}
+		/// <param name="onLoaded">Event to be fired once the download is completed or null</param>
+		/// <returns>The resulting sound if onLoaded is null; otherwise null</returns>
+		public MediaElement DownloadSound(string url, Action<MediaElement> onLoaded)
+        {
+			MediaElement media = new MediaElement();
+			media.Source = new Uri(url);
+
+			media.Loaded += (s, events) =>
+            {
+				if (onLoaded == null) mre.Set();
+				else onLoaded(media);
+			};
+
+			if (onLoaded == null)
+            {
+				mre.WaitOne(1); // FIXME FIXME FIXME TODO
+				return media;
 			}
-			catch { }
 			return null;
 		}
+
 		/// <summary>
 		/// Downloads a map asynchronously
 		/// </summary>
-		/// <param name="url"></param>
+		/// <param name="url">URL where the xml file describing the map is located</param>
 		/// <returns></returns>
-		public void DownloadMap(string url, Action<int, MapInfo> progress) {
+		public void DownloadMap(string url, Action<int, MapInfo> progress)
+        {
 			DownloadString(url, data => {
 				using (XmlReader reader = XmlReader.Create(new StringReader(data))) {
 					MapInfo mapInfo = new MapInfo();
