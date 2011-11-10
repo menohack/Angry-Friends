d2f6401e5adfd1157df13e46739f79f18c6ed072
menohack <menohack@860a1946-0bd5-408d-b3b6-1f7ed0e6db1b>
2011-11-10 01:44:21 +0000

    New branch for rendering hurrrrrrr
    git-svn-id: https://alexander-schiffhauer.svn.beanstalkapp.com/oose-game@182 860a1946-0bd5-408d-b3b6-1f7ed0e6db1b

---
+++ branches/james-doverspike/hurrr/Friendly Wars/Engine/Ultilities/Resource.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+namespace Friendly_Wars.Engine.Utilities {
+	public class Resource {
+		/// <summary>
+		/// List of resource types
+		/// </summary>
+		public enum Types { Image, Sound }
+		/// <summary>
+		/// List of resource priorities
+		/// </summary>
+		public enum Priorities { Sync, Async }
+		/// <summary>
+		/// Name of the resource
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Address of the resource
+		/// </summary>
+		public string URL { get; set; }
+		/// <summary>
+		/// Resource type
+		/// </summary>
+		public Types Type { get; set; }
+		/// <summary>
+		/// Resource priority
+		/// </summary>
+		public Priorities Priority { get; set; }
+		/// <summary>
+		/// Constructs a resource
+		/// </summary>
+		/// <param name="url">Address of the resource</param>
+		/// <param name="type">Resource type</param>
+		/// <param name="priority">Resource priority</param>
+		public Resource(string url, string type, string priority) {
+			this.Name = Path.GetFileNameWithoutExtension(url);
+			this.URL = url;
+			this.Type = (Types)Enum.Parse(typeof(Types), type, true);
+			if (priority == null) { // Defaults
+				switch (this.Type) {
+					case Types.Image: this.Priority = Priorities.Sync; break;
+					case Types.Sound: this.Priority = Priorities.Async; break;
+				}
+			}
+			else this.Priority = (Priorities)Enum.Parse(typeof(Priorities), priority, true);
+		}
+	}
+}
