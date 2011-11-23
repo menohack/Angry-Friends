624548dd126b449f00146af4352c377cf5e72cec
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2011-11-23 15:34:30 -0500

    Image-related problems w/ breakpoint
    Here's the current version with what I'm talking about.  How are those values 0??
    
    Signed-off-by: Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>

--- Angry Friends/Client/MainPage.xaml.cs
+++ Angry Friends/Client/MainPage.xaml.cs
@@ -3,6 +3,10 @@ using System.Windows.Controls;
 using Library.Engine.Object;
 using Library.Engine.Utilities;
 using Library.GameLogic;
+using System.Windows.Media.Imaging;
+using Library.Engine.Component.Graphic;
+using System.Collections.Generic;
+using System.Diagnostics;
 namespace Client {
 	public partial class MainPage : UserControl {
 
@@ -30,6 +34,19 @@ namespace Client {
 		/// <param name="percentage">Downloads percentage</param>
 		/// <param name="mapInfo">Partial/Complete Map Information</param>
 		public void progress(int percentage, MapInfo mapInfo) {
+            BitmapImage image = mapInfo.Images["spritesheet"];
+            //canvas.Children.Add(image);
+
+
+            ///WTF?!
+            Debug.WriteLine(image.PixelHeight);
+            Debug.WriteLine(image.PixelWidth);
+
+
+
+            //SpriteSheetLoader.SpriteSheet spriteSheet = new SpriteSheetLoader.SpriteSheet(image, 15, 15, 30, 30);
+            //IList<Frame> frames = SpriteSheetLoader.Instance.GetFramesFromSpriteSheet(spriteSheet, 0, 0, 3, 3);
+
 		///	textBlock1.Text = percentage + "% Downloaded";
 			///if (percentage == 100) {
 				///image1.Source = mapInfo.Images["spritesheet"];
