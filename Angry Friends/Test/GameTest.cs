5686c44ef4cb7c4d990bfa304ecd55b934753e4f
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2011-11-24 00:41:50 -0500

    [TAG] Iteration 5 build
    Build submitted for iteration 5.
    
    Signed-off-by: Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>

---
+++ Angry Friends/Test/GameTest.cs
@@ -0,0 +1,11 @@
+﻿using Library.GameLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Test {
+	[TestClass]
+	public class GameTest {
+		[TestMethod]
+		public void Create() {
+			//Game g = new Game();
+		}
+	}
+}
