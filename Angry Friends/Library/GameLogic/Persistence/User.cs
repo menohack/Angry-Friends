7c332336c14600d73d7afb2aa47899abf810328b
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2011-11-23 21:54:02 -0500

    Comments
    Added comments to uncommented code…
    
    Signed-off-by: Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>

--- Angry Friends/Library/GameLogic/Persistence/User.cs
+++ Angry Friends/Library/GameLogic/Persistence/User.cs
@@ -3,30 +3,54 @@ using System;
 
 namespace Library.GameLogic.Persistence
 {
-	public class User : Player
+    /// <summary>
+    /// User represents someone who uses the Game.  It contains data relating to the User's friends, rank, etc., and contains methods for acting on the game, such as inviting another User to the game.
+    /// </summary>
+	public class User
 	{
+        /// <summary>
+        /// The friends of this User.
+        /// </summary>
 		private List<User> friends;
 
+        /// <summary>
+        /// The rank of this User.
+        /// </summary>
 		private Rank rank;
 
+        /// <summary>
+        /// The number of games played by this User.
+        /// </summary>
 		private int gamesPlayed;
+
+        /// <summary>
+        /// The number of games won by this User.
+        /// </summary>
 		private int gamesWon;
 
-		private List<Item> inventory;
+        /// <summary>
+        /// This user's inventory, which is comrised of items.
+        /// </summary>
+		private IList<Item> inventory;
 
+        /// <summary>
+        /// The amount of currency this User has.
+        /// </summary>
 		private int currency;
 
-		public User(String name, String tag = null) : base(name, tag)
-		{
-		}
+        /// <summary>
+        /// Constructor for this User.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="tag"></param>
+        public User() { }
 
-		public void InviteToPlay(List<User> users)
+        /// <summary>
+        /// Invites a User to play the game.
+        /// </summary>
+        /// <param name="users">A IList of Users to invite to play.</param>
+		public void InviteToPlay(IList<User> users)
 		{
-		}
-
-		public double SellItem(Item item)
-		{
-			return 0.0;
 		}
 	}
 }
--- Angry Friends/Library/GameLogic/Team.cs
+++ Angry Friends/Library/GameLogic/Team.cs
@@ -2,12 +2,24 @@
 
 namespace Library.GameLogic
 {
+    /// <summary>
+    /// Team represents a collection of Players that are on the same side and fighting together.
+    /// </summary>
 	public class Team
 	{
+        /// <summary>
+        /// The number of Players on this team.
+        /// </summary>
 		public int Count { get; private set; }
 
+        /// <summary>
+        /// The Players on this team.
+        /// </summary>
 		public List<Player> Players { get; private set; }
 
+        /// <summary>
+        /// The name of this team.
+        /// </summary>
 		public string Name { get; private set; }
 	}
 }
