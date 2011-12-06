d825395c2509ce237697d030a0e7a70fe210e2a0
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2011-12-06 15:10:01 -0500

    Got the original logic working with Luis's code.
    Signed-off-by: Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>

--- angry-friends/Client/App.xaml.cs
+++ angry-friends/Client/App.xaml.cs
@@ -15,7 +15,7 @@ namespace Client {
 
 		private void Application_Startup(object sender, StartupEventArgs e) {
 			this.RootVisual = new Grid();
-			SwitchTo(new Rooms("ewfiuwh"));
+			SwitchTo(new TestGame());
 		}
 
 		public static void SwitchTo(UserControl control){
