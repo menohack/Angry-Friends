03e997a956b4f3d5c122cfcde4fb8710e4533f96
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2011-12-11 17:03:39 -0500

    Deleted more unused network code
    Signed-off-by: Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>

--- angry-friends/Model/GameLogic/Networking/RoomInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model.Engine.Utilities {
-	public class RoomInfo {
-		public int ID { get; set; }
-		public string[] Players { get; set; }
-		public int Total { get; set; }
-		public override string ToString() { return ID.ToString(); }
-	}
-}
